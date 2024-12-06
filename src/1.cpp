1e0c8270ec691f76616c9014b2b80eece3a6ff56
spencermcarthur <55674059+spencermcarthur@users.noreply.github.com>
2024-12-06 14:52:53 -0600

    Formatting

--- src/1.cpp
+++ src/1.cpp
@@ -5,40 +5,40 @@
 #include <unordered_map>
 #include <vector>
 
-void read_input(char *fp, std::vector<int> &l, std::vector<int> &r) {
-  std::ifstream ifs(fp);
-  int il, ir;
-  while (ifs >> il >> ir) {
-    l.push_back(il);
-    r.push_back(ir);
-  }
+void read_input(char *filename, std::vector<int> &l, std::vector<int> &r) {
+    std::ifstream ifs(filename);
+    int li, ri;
+    while (ifs >> li >> ri) {
+        l.push_back(li);
+        r.push_back(ri);
+    }
 }
 
 int p1(std::vector<int> l, std::vector<int> r) {
-  std::sort(l.begin(), l.end());
-  std::sort(r.begin(), r.end());
-  int res{};
-  for (size_t i = 0; i < l.size(); i++)
-    res += (l[i] < r[i] ? r[i] - l[i] : l[i] - r[i]);
-  return res;
+    std::sort(l.begin(), l.end());
+    std::sort(r.begin(), r.end());
+    int res{};
+    for (size_t i = 0; i < l.size(); i++)
+        res += (l[i] < r[i] ? r[i] - l[i] : l[i] - r[i]);
+    return res;
 }
 
 int p2(std::vector<int> &l, std::vector<int> &r) {
-  std::unordered_map<int, int> r_count;
-  for (int n : r)
-    r_count[n] += 1;
+    std::unordered_map<int, int> r_count;
+    for (int n : r) r_count[n] += 1;
 
-  int res{};
-  for (int n : l)
-    res += (n * r_count[n]);
+    int res{};
+    for (int n : l) res += (n * r_count[n]);
 
-  return res;
+    return res;
 }
 
 int main(int argc, char *argv[]) {
-  std::vector<int> l{}, r{};
-  read_input(argv[1], l, r);
+    if (argc != 2) return 1;
 
-  std::cout << "Part 1: " << p1(l, r) << "\n";
-  std::cout << "Part 2: " << p2(l, r) << "\n";
+    std::vector<int> left{}, right{};
+    read_input(argv[1], left, right);
+
+    std::cout << "Part 1: " << p1(left, right) << "\n";
+    std::cout << "Part 2: " << p2(left, right) << "\n";
 }
